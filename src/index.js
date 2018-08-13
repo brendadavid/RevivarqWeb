efc81249916a93f40db90870ee7bb57008009de9
Leonardo Marcelino Vieira <leonardo.marcelino@outlook.com>
2018-08-13 14:01:44 -0300

    Roboto Font, Absolute path

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './boot/App';
-import registerServiceWorker from './registerServiceWorker';
+import 'styles/index.css';
+import App from 'boot/App';
+import registerServiceWorker from 'registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+	<App />, document.getElementById('root')
+);
 registerServiceWorker();
